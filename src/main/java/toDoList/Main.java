aa28539074512cd9d557ba2b50837e1e01e0d3e6
Artem Tratsiuk <inqui05@gmail.com>
2015-03-29 23:38:16 +0300

    Initial commit

---
+++ src/main/java/toDoList/Main.java
@@ -0,0 +1,11 @@
+package main.java.toDoList;
+
+/**
+ * User: inqui05
+ * Date: 29.03.2015
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Hello my future application");
+    }
+}
